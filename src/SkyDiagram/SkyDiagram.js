b1cf819fb4e80d72e87ea47a2fe0857d03a4e1b6
Chris Siedell <chris@siedell.com>
2019-05-15 19:19:56 -0500

    Added tangent angle to path position calculation

--- src/SkyDiagram/SkyDiagram.js
+++ src/SkyDiagram/SkyDiagram.js
@@ -2,7 +2,7 @@
 SkyDiagram
 wgbh/asset1
 astro.unl.edu
-2019-05-12
+2019-05-15
 */
 
 import './SkyDiagram.css';
@@ -37,12 +37,13 @@ export default class SkyDiagram {
 			horizon: 0.2,
 			peak: 0.8,
 			margin: 0.1,
-			//path: [{x: 0.5, y: 1.0}],
-			path: [{x: 0, y: 0.75}, {x: 0.125, y: 0.875}, {x: 0.25, y: 1.0}],
-			//path: [{x: 0, y: 1}],
 
-			sunPathPosition: 0.5,
-			moonPathPosition: 0.2,
+			//path: [{x: 0.5, y: 1.0}],
+			//path: [{x: 0, y: 0.75}, {x: 0.125, y: 0.875}, {x: 0.25, y: 1.0}],
+			path: [{x: 0, y: 1}],
+
+			sunPathPosition: 0.2,
+			moonPathPosition: 0.4,
 		};
 
 		this.setParams(this._defaultParams);
@@ -136,11 +137,17 @@ export default class SkyDiagram {
 
 	validatePath(arg) {
 		if (arg.length % 2 !== 1) {
-			throw new Error('path array must have odd length.');
+			throw new Error('The path array must have odd length.');
 		}
 		var copy = [];
 		for (let i = 0; i < arg.length; ++i) {
 			let pt = arg[i];
+			if (typeof pt.x !== 'number') {
+				pt.x = Number(pt.x);
+			}
+			if (typeof pt.y !== 'number') {
+				pt.y = Number(pt.y);
+			}
 			if (!Number.isFinite(pt.x) || !Number.isFinite(pt.y)) {
 				throw new Error('Each object in the path array must have valid x and y properties.');
 			}
@@ -160,6 +167,9 @@ export default class SkyDiagram {
 	}
 
 	validateObjectPathPosition(arg) {
+		if (typeof arg !== 'number') {
+			arg = Number(arg);
+		}
 		if (!Number.isFinite(arg)) {
 			throw new Error('A path position must be a finite number.');
 		}
@@ -271,6 +281,12 @@ export default class SkyDiagram {
 
 		let startTime = performance.now();
 
+		// The path is defined by the _path property. This property is an array of points (objects
+		//  with x and y properties) defining the shape of the objects' path (i.e. the celestial
+		//  horizon).
+
+		// TODO: document
+
 		// These constants are used to transform a path definition point to a screen point. 
 		//  Path definition space: right-hand system with origin at lower left path/horizon
 		//		intersection and <1,1> at zenith.
@@ -294,33 +310,28 @@ export default class SkyDiagram {
 			p[i].y = pYScale*p[i].y + pYOffset;
 		}
 
-		// _segments will contain the piecewise definition of the sun/moon path starting at the left
+		// _curves will contain the quadratic bezier curves defining the sun/moon path starting at the left
 		//  horizon point, going up to the zenith, and then going back down to the right horizon point.
-		// Each object in _segments has these properties:
-		//  a0, a1: the starting and ending anchor points (the ending anchor point of one segment will
-		//					be identical to the starting anchor point of the next segment),
-		// 	cq: the control point for the quadratic bezier (from definition),
-		//  cc0, cc1: the control points for the cubic bezier (derived).
-		// All segment points are objects with x and y properties, defined in screen space (LHS).
-		this._segments = [];
+		// Each object in _curves has these properties:
+		//  a0, a1: the starting and ending anchor points (the ending anchor point of one curve will
+		//					be identical to the starting anchor point of the next curve),
+		// 	c: the control point for the quadratic bezier.
+		// All curve points are objects with x and y properties, defined in screen space (LHS).
+		this._curves = [];
 
 		if (p.length % 2 !== 1) {
 			throw new Error('Assert failed: p.length must be odd.');
 		}
 
-		let numSegments = (p.length - 1) / 2;
+		let numCurves = (p.length - 1) / 2;
 		let pIndex = 0;
 
-		for (let n = 0; n < numSegments; ++n) {
-			let segment = {};
-			segment.a0 = p[pIndex++];
-			segment.qc = p[pIndex++];
-			segment.a1 = p[pIndex];
-			segment.cc0 = {	x: segment.a0.x + (2/3)*(segment.qc.x - segment.a0.x),
-											y: segment.a0.y + (2/3)*(segment.qc.y - segment.a0.y)};
-			segment.cc1 = { x: segment.a1.x + (2/3)*(segment.qc.x - segment.a1.x),
-											y: segment.a1.y + (2/3)*(segment.qc.y - segment.a1.y)};
-			this._segments.push(segment);
+		for (let n = 0; n < numCurves; ++n) {
+			let curve = {};
+			curve.a0 = p[pIndex++];
+			curve.c = p[pIndex++];
+			curve.a1 = p[pIndex];
+			this._curves.push(curve);
 		}
 
 
@@ -334,30 +345,19 @@ export default class SkyDiagram {
 		}
 
 		// Mirror the left quadrant to the right.
-		for (let i = this._segments.length - 1; i >= 0; --i) {
-			let left = this._segments[i];
+		for (let i = this._curves.length - 1; i >= 0; --i) {
+			let left = this._curves[i];
 
-			let segment = {};
-			segment.a0 = getMirroredPoint(left.a1);
-			segment.qc = getMirroredPoint(left.qc);
-			segment.a1 = getMirroredPoint(left.a0);
-			segment.cc0 = getMirroredPoint(left.cc1);
-			segment.cc1 = getMirroredPoint(left.cc0);
+			let curve = {};
+			curve.a0 = getMirroredPoint(left.a1);
+			curve.c = getMirroredPoint(left.c);
+			curve.a1 = getMirroredPoint(left.a0);
 
-			this._segments.push(segment);
+			this._curves.push(curve);
 		}
 
-		// Calculate the lookup tables for the segments.
+		// Calculate the lookup tables for the curves.
 		this._calcPathLookup();
-
-		// Determine the total path length (has to be done after _calcPathLookup).
-		// The path length from the left horizon, up to the zenith, and then back down
-		//  to the right horizon defined as exactly half the total path length.
-		let halfPathLength = 0;
-		for (let i = 0; i < this._segments.length; ++i) {
-			halfPathLength += this._segments[i].length;
-		}
-		this._totalPathLength = 2 * halfPathLength;
 
 		// Objects below the horizon will follow a linear path that continues from
 		//  the horizon intersection point. This line is defined by the horizon intersection
@@ -366,11 +366,18 @@ export default class SkyDiagram {
 		//  intersection. The left and right 'nadir' points will not be the same.
 
 		// ldx, ldy: left horizon point derivative
-		// lbha: left below horizon angle
-		let s = this._segments[0];
-		let ldx = 2*(s.qc.x - s.a0.x);
-		let ldy = 2*(s.qc.y - s.a0.y);
-		let lbha = Math.atan2(ldy, ldx) + Math.PI;
+		// lbha: left below horizon angle, the angle going down (not the path tangent)
+		let s = this._curves[0];
+		let ldx = 2*(s.c.x - s.a0.x);
+		let ldy = 2*(s.c.y - s.a0.y);
+
+		this._leftBelowHorizonTangentAngle = Math.atan2(ldy, ldx);
+
+		let lbha = this._leftBelowHorizonTangentAngle + Math.PI;
+
+
+		this._rightBelowHorizonTangentAngle = Math.PI - lbha;
+
 
 		this._leftHorizonPt = {x: s.a0.x, y: s.a0.y};
 		this._leftNadirPt = {};
@@ -400,6 +407,17 @@ export default class SkyDiagram {
 		ctx.ellipse(sunPt.x, sunPt.y, 10, 10, 0, 0, 2*Math.PI);
 		ctx.fill();
 
+		let ca = Math.cos(sunPt.angle);
+		let sa = Math.sin(sunPt.angle);
+		let r = 20;
+
+		ctx.strokeStyle = 'rgb(255, 140, 140)';
+		ctx.lineWidth = 5;
+
+		ctx.beginPath();
+		ctx.moveTo(sunPt.x, sunPt.y);
+		ctx.lineTo(sunPt.x + r*ca, sunPt.y + r*sa);
+		ctx.stroke();
 		
 		this._needsUpdatePathPositions = false;
 	}
@@ -409,7 +427,7 @@ export default class SkyDiagram {
 		let startTime = performance.now();
 
 		// TODO: check whether improved algorithm efficiency is warranted (search for position
-		// above horizon always starts at 0 and goes up linearly)
+		// above horizon always starts at 0 and goes up linearly, both in _curves and in lookup table)
 
 		// This method returns a screen point for a path position. The fractional part of the
 		//  position argument specifies a position along the path. Some key positions:
@@ -424,7 +442,7 @@ export default class SkyDiagram {
 			throw new Error('A path position must be a finite number.');
 		}
 
-		// position in [0, 1)
+		// Require position to be in [0, 1).
 		position = (position%1 + 1)%1;
 
 		let pt = {};
@@ -433,38 +451,40 @@ export default class SkyDiagram {
 			// Above the horizon.
 
 			// dist is the distance along the path to the position, starting
-			//  from the left horizon point.
+			//  from the left horizon point, going up.
 			const dist = position * this._totalPathLength;
 
-			// totalDist: the total distance traversed along path so far
+			// totalDist: the total distance traversed along the path so far.
 			let totalDist = 0;
 
 			// Find the curve containing the path position.
-			let segment = this._segments[0];
-			for (let i = 1; i < this._segments.length; ++i) {
+			let curve = this._curves[0];
+			for (let i = 1; i < this._curves.length; ++i) {
 
-				// nextTotalDist: next totalDist, if advancing to the next curve
-				let nextTotalDist = totalDist + segment.length;
+				// nextTotalDist: next totalDist, if advancing to the next curve.
+				let nextTotalDist = totalDist + curve.length;
 
 				if (dist < nextTotalDist) {
-					// The next curve starts after the position.
+					// The next curve starts after the position, so we've found the
+					//  curve to use.
 					break;
 				}
 
-				segment = this._segments[i];
+				// Advance to the next curve. 
+				curve = this._curves[i];
 				totalDist = nextTotalDist;
 			}
 
-			let lookup = segment.lookup;
+			let lookup = curve.lookup;
 
-			// ts: the total length along this curve to the position.
+			// ts: the total length along the given curve to the position.
 			let ts = dist - totalDist;
 
-			// Find the entry in the lookup table for the interval containing the position.
+			// Find the entry in the lookup table for the sampled interval containing the position.
 			let entry = lookup[0];
 			for (let i = 1; i < lookup.length; ++i) {
 				if (ts < lookup[i].ts) {
-					// The next interval starts after the position.
+					// The next interval starts after the position, so we're done.
 					break;
 				}
 				entry = lookup[i];
@@ -473,16 +493,24 @@ export default class SkyDiagram {
 			// Calculate the bezier parameter for the position.
 			let u = entry.u + entry.du*((ts - entry.ts) / entry.s);
 			
-			let a0 = segment.a0;
-			let qc = segment.qc;
-			let a1 = segment.a1;
+			let a0 = curve.a0;
+			let c = curve.c;
+			let a1 = curve.a1;
 
 			let A = (1 - u)*(1 - u);
 			let B = 2*(1 - u)*u;
 			let C = u*u;
 
-			pt.x = A*a0.x + B*qc.x + C*a1.x;
-			pt.y = A*a0.y + B*qc.y + C*a1.y;
+			let D = 2*(1 - u);
+			let E = 2*u;
+
+			pt.x = A*a0.x + B*c.x + C*a1.x;
+			pt.y = A*a0.y + B*c.y + C*a1.y;
+
+			let dx = D*(c.x - a0.x) + E*(a1.x - c.x);
+			let dy = D*(c.y - a0.y) + E*(a1.y - c.y);
+
+			pt.angle = Math.atan2(dy, dx);
 
 		} else {
 			// Below the horizon.
@@ -495,13 +523,14 @@ export default class SkyDiagram {
 				// Below the horizon, on the right.
 				pt.x = this._rightHorizonPt.x + quadPos*(this._rightNadirPt.x - this._rightHorizonPt.x);
 				pt.y = this._rightHorizonPt.y + quadPos*(this._rightNadirPt.y - this._rightHorizonPt.y);
+				pt.angle = this._rightBelowHorizonTangentAngle;
 			} else {
 				// Below the horizon, on the left.
 				pt.x = this._leftNadirPt.x + quadPos*(this._leftHorizonPt.x - this._leftNadirPt.x);
 				pt.y = this._leftNadirPt.y + quadPos*(this._leftHorizonPt.y - this._leftNadirPt.y);
+				pt.angle = this._leftBelowHorizonTangentAngle;
 			}
 		}
-
 
 		let endTime = performance.now();
 
@@ -517,12 +546,31 @@ export default class SkyDiagram {
 		//  the path. It also generates lookup tables for each curve. These tables are used for assigning
 		//  a position that is based on the actual path length, and not the bezier parameter (u).
 
+		// This method requires that _curves be populated with the curves that define the path from
+		//  the left horizon point, up to the zenith, then back down to the right horizon point. It adds
+		//  a lookup property (an array of entries, see below) to each of these curve objects. It also
+		//  sets the component's _totalPathLength property.
+
+		// The path length and lookup tables do not need to be very accurate -- the path is drawn using
+		//  bezier parameters (i.e. it doesn't rely on these values), and an object's position on the
+		//  path is also determined using the bezier curve formula. The values calculated here
+		//  are used just to determine which bezier curve a path position is on, and the bezier parameter
+		//  on that curve corresponding to that position (see getScreenPointForPathPosition). The only
+		//  essential requirement is that all the pieces in all the lookup tables sum up to half the
+		//  calculated path length -- which this method guarantees is true. A lack of precision in the
+		//  lookup tables will just introduce some non-linear distortions in an object's movement along
+		//  the path.
+
 		// minS and maxS define the target range (in pixels) for the piecewise linear approximation
-		//  of the bezier curve. The distance between sampled points must be less than maxS -- the
+		//  of each bezier curve. The distance between sampled points must be less than maxS -- the
 		//  algorithm will keep reducing du until this is achieved. The distance may be less than
 		//  minS, in which case the algorithm will try increasing du for the next sample. 
-		const maxS = 1.5;
-		const minS = 0.5;
+		// Recommendation: set maxS > 2 * minS (at least), since du is either doubled or halved.
+		const maxS = 12;
+		const minS = 5;
+
+		// All the curves will sum up to half the path length (the other half is below the horizon).	
+		let halfPathLength = 0;
 
 		function dist(p1, p2) {
 			let dx = p2.x - p1.x;
@@ -530,12 +578,12 @@ export default class SkyDiagram {
 			return Math.sqrt(dx*dx + dy*dy);
 		}
 
-		for (let i = 0; i < this._segments.length; ++i) {
+		for (let i = 0; i < this._curves.length; ++i) {
 
-			let segment = this._segments[i];
-			let a0 = segment.a0;
-			let qc = segment.qc;
-			let a1 = segment.a1;
+			let curve = this._curves[i];
+			let a0 = curve.a0;
+			let c = curve.c;
+			let a1 = curve.a1;
 
 			function curvePt(u) {
 				let A = (1 - u)*(1 - u);
@@ -543,26 +591,26 @@ export default class SkyDiagram {
 				let C = u*u;
 
 				let pt = {};
-				pt.x = A*a0.x + B*qc.x + C*a1.x;
-				pt.y = A*a0.y + B*qc.y + C*a1.y;
+				pt.x = A*a0.x + B*c.x + C*a1.x;
+				pt.y = A*a0.y + B*c.y + C*a1.y;
 				return pt;
 			}
 
-			// Each segment has a lookup table assigned to it. This table consists of sampled
+			// Each curve has a lookup table assigned to it. This table consists of sampled
   		//  points along the curve. 
 			// Properties of each entry in the lookup table:
-			//  u - bezier parameter, monotonically increasing over the samples, in range [0, 1)
-			//  du - delta u to next sample
-			//  s - linear distance, in pixels, to next sample
-			//  ts - total length of curve (by linear approx) up to the sampled point (is 0 for first entry)
-			//  pt - sampled point, an object with x and y properties (in screen space)
+			//  u: bezier parameter, monotonically increasing over the samples, in range [0, 1),
+			//  du: delta u to next sample,
+			//  s: linear distance, in pixels, to next sample,
+			//  ts: total length of curve (by linear approx) up to the sampled point (is 0 for first entry),
+			//  pt: sampled point, an object with x and y properties (in screen space).
 
 			let u = 0;
 			let du = 0.5;
 			
 			let lookup = [];
 
-			let pt = {x: segment.a0.x, y: segment.a0.y};
+			let pt = {x: curve.a0.x, y: curve.a0.y};
 			let npt, nu, s;
 
 			let ts = 0;
@@ -584,6 +632,7 @@ export default class SkyDiagram {
 					s = dist(pt, npt);
 
 					// If the distance exceeds the desired limit reduce the delta and try again.
+					//  Otherwise, we're done.
 					if (s > maxS) {
 						du *= 0.5;
 						continue;
@@ -600,18 +649,21 @@ export default class SkyDiagram {
 				u = nu;
 				pt = npt;
 
-				// If the distance was small try a larger delta next time.
+				// If the distance was short try a larger delta next time.
 				if (s < minS) {
 					du *= 1.5;
 				}
 			}
 
-			segment.lookup = lookup;
-			segment.length = ts;
+			curve.lookup = lookup;
+			curve.length = ts;
+			
+			halfPathLength += ts;
 		}
 
-
+		this._totalPathLength = 2 * halfPathLength;
 	}
+
 
 	_redraw() {
 
@@ -619,31 +671,37 @@ export default class SkyDiagram {
 
 		let ctx = this._skyCanvas.getContext('2d');
 
-		ctx.fillStyle = 'rgb(200, 200, 250)';
+		ctx.fillStyle = 'rgb(200, 200, 200)';
 		ctx.fillRect(0, 0, this._contentWidth, this._contentHeight);
 
-		ctx.fillStyle = 'rgb(150, 90, 90)';
+		ctx.fillStyle = 'rgb(90, 90, 90)';
 		ctx.fillRect(0, this._groundOrigin, this._contentWidth, this._groundHeight);
 
-		ctx.strokeStyle = 'rgb(235, 235, 255)';
+		ctx.strokeStyle = 'rgb(235, 235, 235)';
 		ctx.lineWidth = 2;
 
-		let s = this._segments[0];
+		let s = this._curves[0];
 		ctx.beginPath();
 		ctx.moveTo(s.a0.x, s.a0.y);
-		for (let i = 0; i < this._segments.length; ++i) {
-			let s = this._segments[i];
-			ctx.quadraticCurveTo(s.qc.x, s.qc.y, s.a1.x, s.a1.y);
+		for (let i = 0; i < this._curves.length; ++i) {
+			let s = this._curves[i];
+			ctx.quadraticCurveTo(s.c.x, s.c.y, s.a1.x, s.a1.y);
+
+//			// lookup table testing
+//			let lookup = s.lookup;
+//			for (let j = 0; j < lookup.length; j++) {
+//				ctx.lineTo(lookup[j].pt.x, lookup[j].pt.y);
+//			}
 		}
 		ctx.stroke();
 
-		ctx.beginPath();
-		ctx.strokeStyle = 'rgb(200, 255, 200)';
-		ctx.moveTo(this._leftHorizonPt.x, this._leftHorizonPt.y);
-		ctx.lineTo(this._leftNadirPt.x, this._leftNadirPt.y);
-		ctx.moveTo(this._rightHorizonPt.x, this._rightHorizonPt.y);
-		ctx.lineTo(this._rightNadirPt.x, this._rightNadirPt.y);
-		ctx.stroke();
+//		ctx.beginPath();
+//		ctx.strokeStyle = 'rgb(200, 255, 200)';
+//		ctx.moveTo(this._leftHorizonPt.x, this._leftHorizonPt.y);
+//		ctx.lineTo(this._leftNadirPt.x, this._leftNadirPt.y);
+//		ctx.moveTo(this._rightHorizonPt.x, this._rightHorizonPt.y);
+//		ctx.lineTo(this._rightNadirPt.x, this._rightNadirPt.y);
+//		ctx.stroke();
 		
 	}
 
